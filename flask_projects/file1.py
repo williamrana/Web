b06b3b0d5212de4198bcdaf62f1e05c941d0972f
Vimal Rana <williamrana@gmail.com>
2020-09-25 12:24:42 +0530

    Add files via upload

---
+++ flask_projects/file1.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    print("file is run directly")
+else:
+    print("file is imported",__name__)
+
---
+++ flask_projects/file2.py
@@ -0,0 +1,7 @@
+import file1
+
+if __name__ == "__main__":
+    print("file is run directly")
+else:
+    print("file is imported")
+
